676c857bb97b9295100e607a1ccebc7b7d973cf0
hakim <h.benhlouli@free.fr>
2023-06-05 16:03:53 +0200

    porto

--- script.js
+++ script.js
@@ -1,20 +1,75 @@
 "use strict"
 
-$(document).ready(function () {
-    // typing animation
-    (function ($) {
-      $.fn.writeText = function (content) {
-        var contentArray = content.split(""),
-          current = 0,
-          elem = this;
-        setInterval(function () {
-          if (current < contentArray.length) {
-            elem.text(elem.text() + contentArray[current++]);
-          }
-        }, 80);
-      };
-    })(jQuery);
+// init
+var maxx = document.body.clientWidth;
+var maxy = document.body.clientHeight;
+var halfx = maxx / 2;
+var halfy = maxy / 2;
+var canvas = document.createElement("canvas");
+document.body.appendChild(canvas);
+canvas.width = maxx;
+canvas.height = maxy;
+canvas.style.position = "fixed";
+canvas.style.top = 0;
+var context = canvas.getContext("2d");
+var dotCount = 200;
+var dots = [];
+// create dots
+for (var i = 0; i < dotCount; i++) {
+  dots.push(new dot());
+}
+
+// dots animation
+function render() {
+  context.fillStyle = "#000000";
+  // context.fillRect(0, 0, maxx, maxy);
+  context.clearRect(0,0,maxx, maxy)
+  for (var i = 0; i < dotCount; i++) {
+    dots[i].draw();
+    dots[i].move();
+  }
+  requestAnimationFrame(render);
+}
+
+// dots class
+// @constructor
+function dot() {
   
-    // input text for typing animation
-    $("#holder").writeText("WEB DESIGNER + FRONT-END DEVELOPER");
-})
+  this.rad_x = 2 * Math.random() * halfx + 1;
+  this.rad_y = 1.2 * Math.random() * halfy + 1;
+  this.alpha = Math.random() * 360 + 1;
+  this.speed = Math.random() * 100 < 50 ? 1 : -1;
+  this.speed *= 0.1;
+  this.size = Math.random() * 5 + 1;
+  this.color = Math.floor(Math.random() * 256);
+  
+}
+
+// drawing dot
+dot.prototype.draw = function() {
+  
+  // calc polar coord to decart
+  var dx = halfx + this.rad_x * Math.cos(this.alpha / 180 * Math.PI);
+  var dy = halfy + this.rad_y * Math.sin(this.alpha / 180 * Math.PI);
+  // set color
+  context.fillStyle = "rgb(" + this.color + "," + this.color + "," + this.color + ")";
+  // draw dot
+  context.fillRect(dx, dy, this.size, this.size);
+  
+};
+
+// calc new position in polar coord
+dot.prototype.move = function() {
+  
+  this.alpha += this.speed;
+  // change color
+  if (Math.random() * 100 < 50) {
+    this.color += 1;
+  } else {
+    this.color -= 1;
+  }
+  
+};
+
+// start animation
+render();
